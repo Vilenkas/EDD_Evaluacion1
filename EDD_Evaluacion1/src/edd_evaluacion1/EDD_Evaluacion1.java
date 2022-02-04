2dbe166338104b4debcf0d5b1ec0f103c8fc428a
Gabriel Villegas <gabriellarez7@gmail.com>
2022-02-04 15:58:54 -0430

    Commit Inicial

---
+++ EDD_Evaluacion1/src/edd_evaluacion1/EDD_Evaluacion1.java
@@ -0,0 +1,17 @@
+
+package edd_evaluacion1;
+
+/**
+ * @author Gabriel Villegas
+ * @version 04/02/2022
+ */
+public class EDD_Evaluacion1 {
+
+    /**|
+     * @param args Argumentos en línea de consola
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
